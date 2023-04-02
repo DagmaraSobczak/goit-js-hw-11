f1e91af6c40883fe559d347688ff9f21962fe088
DagmaraSobczak <daga.sobczak@gmail.com>
2023-04-02 23:00:44 +0200

    Added pagination and a "load more" function.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import SimpleLightbox from 'simplelightbox';
 
 const dataSubmit = document.querySelector('.button');
 const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
 
-async function fetchData(searchTerm) {
+async function fetchData(searchTerm, page) {
   try {
     const response = await axios.get('https://pixabay.com/api/', {
       params: {
@@ -14,17 +15,23 @@ async function fetchData(searchTerm) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: 40,
+        page: page,
       },
     });
 
     const images = response.data.hits;
+    const totalHits = response.data.totalHits;
     if (images.length === 0) {
       Notiflix.Notify.info(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
-    gallery.innerHTML = '';
+    if (page === 1) {
+      gallery.innerHTML = '';
+    }
+
     images.forEach(image => {
       const galleryItem = document.createElement('div');
       galleryItem.classList.add('photo-card');
@@ -63,7 +70,19 @@ async function fetchData(searchTerm) {
 
       gallery.appendChild(galleryItem);
     });
-
+    if (currentPage === 1) {
+      loadMore.style.display = 'block';
+    } else if (
+      images.length < 40 ||
+      images.length + (currentPage - 1) * 40 === totalHits
+    ) {
+      loadMore.style.display = 'none';
+      Notiflix.Notify.info(
+        'We are sorry, but you have reached the end of search results.'
+      );
+    } else {
+      loadMore.style.display = 'block';
+    }
     new SimpleLightbox('.gallery a', {
       captionsData: 'alt',
       captionDelay: 250,
@@ -72,16 +91,31 @@ async function fetchData(searchTerm) {
     console.error(error);
   }
 }
+let currentPage = 1;
+let currentSearchTerm = '';
 
 dataSubmit.addEventListener('click', event => {
   event.preventDefault();
   const searchTerm = document.querySelector('#search-form input').value;
   if (searchTerm === '') {
+    loadMore.style.display = 'none';
     Notiflix.Notify.info(
       'Sorry, there are no images matching your search query. Please try again.'
     );
     return;
   }
+  if (searchTerm !== currentSearchTerm) {
+    currentPage = 1;
+    currentSearchTerm = searchTerm;
+    loadMore.classList.remove('visible');
+  } else {
+    currentPage++;
+  }
 
-  fetchData(searchTerm);
+  fetchData(currentSearchTerm, currentPage);
 });
+function loadMoreImg() {
+  currentPage++;
+  fetchData(currentSearchTerm, currentPage);
+}
+loadMore.addEventListener('click', loadMoreImg);
