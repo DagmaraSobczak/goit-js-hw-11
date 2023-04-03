a63a9f12f13e6b4e2f5b3c73d5e25c171db2d5d6
DagmaraSobczak <daga.sobczak@gmail.com>
2023-04-03 19:58:52 +0200

    imported simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const dataSubmit = document.querySelector('.button');
 const gallery = document.querySelector('.gallery');
